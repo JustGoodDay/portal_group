a0174a56be65fbcc1d98416c5ed3a9e33c8168aa
vusen <vusenko080809@gmail.com>
2025-09-27 16:27:00 +0200

    10 пункт портфоліо
    модификація юзера

--- core/settings.py
+++ core/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication_app',
-    'forum_app',
+    'portfolio',
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,7 +60,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/"templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- core/urls.py
+++ core/urls.py
@@ -1,32 +1,7 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from authentication_app.views import *
-from forum_app.views import *
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('register/', register_view, name='register'),
-# Форум
-    path('forum/', forum_list, name='forum_list'),
-    path('forum/new/', forum_create, name='forum_create'),
-# Жалобы
-    path('complaints/', complaint_list, name='complaint_list'),
-    path('complaints/new/', complaint_create, name='complaint_create'),
+    path("admin/", admin.site.urls),
+    path("auth/", include("authentication_app.urls")),
 ]
-
